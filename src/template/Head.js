1475989c55a43d6fbf973f4facc38ca127eca481
Bruno Sorban <brunosorban@usp.br>
2021-12-11 14:29:16 -0300

    hotfix: de volta ao vacinei 2

--- src/template/Head.js
+++ src/template/Head.js
@@ -6,7 +6,7 @@ function Head() {
         <div id='header-title'>
             <img src="https://i.imgur.com/6PHyWdC.png" alt="logo"/>
             <h1 id='title'>Vacinei Dashboard</h1>
-            <a href='https://site-vacinei.herokuapp.com/'><h3>De volta ao Vacinei</h3></a>
+            <h3 id='link-volta'><a href='https://site-vacinei.herokuapp.com/'>De volta ao Vacinei</a></h3>
         </div>
     </header>
     );
